c114166c6fc3b660b7dcd892911a9ea5fb3cb6aa
Marly Diallo <marly.diallo2021@gmail.com>
2023-12-11 19:38:56 -0500

    routes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-<h1>Marly Yasuke e-commecre</h1>
-{/* navbar */}
-      </header>
+    <div>
+<BrowserRouter>
+{/* header + navbar */}
+<Routes>
 
+</Routes>
+</BrowserRouter>
     </div>
   );
 }
