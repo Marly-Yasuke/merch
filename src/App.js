af9c27c2c7dbcc7ca705a668a19b162891153b5f
Marly Diallo <marly.diallo2021@gmail.com>
2023-12-11 19:52:37 -0500

    routing + pages setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Contact from './pages/Contact';
+import About from './pages/About';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
 {/* header + navbar */}
 <Routes>
 <Route path="/" element={<Home />}/>
+<Route path="/shop" element={<Shop />}/>
+<Route path="/contact" element={<Contact />}/>
+<Route path="/about" element={<About />}/>
 </Routes>
 </BrowserRouter>
     </div>
